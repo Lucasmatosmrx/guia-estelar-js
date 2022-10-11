904e61a6661ca5b047e4fbb4e84c5e88812ed56e
Lucas Matos <lucas.suporte.tec@outlook.com>
2022-10-10 20:58:38 -0400

    add script comentarios

---
+++ comentarios.js
@@ -0,0 +1,11 @@
+//Aqui eu tenho um comentaraio em linha
+
+/*
+    Aqui eu tenho um comentario multi linha
+
+    o codigo abaixo escreve alguma mensagem no devtools
+    
+*/
+
+//o codigo abaixo escreve alguma mensagem no console
+console.log("Bem Vindo ao mundo do Lucas");

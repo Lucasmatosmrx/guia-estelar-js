f4f19387cdda3a080075dbfbdfd894eaa8b0c240
Lucas Matos <lucas.suporte.tec@outlook.com>
2022-10-11 21:59:29 -0400

    :bug: add boolean, number, string

---
+++ 2 - tipos de dados/number.js
@@ -0,0 +1,19 @@
+/*
+    Number
+    
+    *numeros
+
+    33 // inteiros
+    12.5 // reais
+    NaN // Not a Number
+    Infinity // infinito
+
+*/
+
+console.log(33);
+
+console.log(12.5 + 33);
+
+console.log(12.5 / "asd");
+
+console.log(12.5 === Infinity);
---
+++ 2 - tipos de dados/string.js
@@ -0,0 +1,23 @@
+/*
+    String
+
+    * Cadeia de caracteres
+
+    "" // aspas duplas
+    '' // aspas simples
+    `` // template literals ou template strings
+
+*/
+
+//string
+console.log("Teste");
+
+//com crase podemos ter espaço entre as linhas
+console.log(`
+
+crase
+
+`);
+
+//com crase podemos ter expressões de linguagem que se chama de interpolação
+console.log(`teste ${1 + 1}`);

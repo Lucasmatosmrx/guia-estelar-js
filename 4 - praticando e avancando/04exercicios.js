e016401d31d5fc1a785f6a745a1b0021d528e7ec
Lucas Matos <lucas.suporte.tec@outlook.com>
2022-10-13 23:44:46 -0400

    :bug: add functions/exercicios

---
+++ 4 - praticando e avancando/04exercicios.js
@@ -0,0 +1,100 @@
+//1 - Declare uma variavel de nome weight
+//let weight;
+
+//2 - Que tipo de dado é a variavel acima?
+//console.log(typeof weight);
+
+/*
+  3. Declare uma variavel e atribua valores para cada
+  um dos dados:
+    * name: String
+    * age: Number(integer)
+    * stars: Number(float)
+    * isSubscribed: Boolean
+
+*/
+
+//let name = "Lucas";
+//let age = 50;
+//let start = 3.5;
+//let isSubscribed = true;
+
+/*
+  4. A variavel student abaixo é de que tipo de dados?
+
+  4.1 Atribua a ela as mesmas propriedades e valores do exercicio 3.
+
+  4.2 Mostre no console a seguinte mensagem: 
+
+    <name> de idade <age> pensa <weight> kg.
+
+    Atenção, substitua <name> <age> e <weight> pelos valores de cada propriedade do objeto.
+
+*/
+
+let student = {
+  name: "Lucas",
+  age: 50,
+  weight: 73.5,
+  isSubscribed: true,
+};
+
+// console.log(typeof student);
+
+// console.log(
+//   `${student.name} tem idade de ${student.age} pesa ${student.weight} kg.`
+// );
+
+/*
+  5. Declare uma variavel do tipo Array, de nome students e atribua a ela nenhum valor,
+  ou seja, somente o Array vazio.
+
+*/
+
+let students = [];
+
+/*
+  6. Reatribua valor para a variável acima, colocando
+  dentro dela o objeto student da questão 4. (Não copiar
+  e colar o objeto, mas usar o objeto criado e inserir ele no
+  Array.)
+*/
+
+students = [student];
+
+//console.log(students);
+
+/*
+    7. Coloque no console o valor da posição zero do Array acima
+*/
+
+//console.log(students[0]);
+
+/*
+    8. Crei um novo student e coloque na posição 1 do Array students.
+*/
+
+// const john = {
+//   name: "Maria",
+//   age: 70,
+//   weight: 75,
+//   isSubscribed: true,
+// };
+
+// students[1] = john;
+
+// console.log(students);
+
+/*
+    9. Sem rodar o código responda qual é a resposta do código abaixo
+    e por que? Após sua resposta, rode o código e veja se você acertou
+
+    console.log(a)
+    var a = 1
+ */
+
+console.log(a);
+var a = 1;
+
+console.log(b);
+let b = 1;
--- script.js
+++ script.js
@@ -1,52 +1,11 @@
-//1 - Declare uma variavel de nome weight
-//let weight;
+// function expression
+// function anonymous
 
-//2 - Que tipo de dado é a variavel acima?
-//console.log(typeof weight);
-
-/*
-  3. Declare uma variavel e atribua valores para cada
-  um dos dados:
-    * name: String
-    * age: Number(integer)
-    * stars: Number(float)
-    * isSubscribed: Boolean
-
-*/
-
-//let name = "Lucas";
-//let age = 50;
-//let start = 3.5;
-//let isSubscribed = true;
-
-/*
-  4. A variavel student abaixo é de que tipo de dados?
-
-  4.1 Atribua a ela as mesmas propriedades e valores do exercicio 3.
-
-  4.2 Mostre no console a seguinte mensagem: 
-
-    <name> de idade <age> pensa <weight> kg.
-
-    Atenção, substitua <name> <age> e <weight> pelos valores de cada propriedade do objeto.
-
-*/
-
-let student = {
-  name: "Lucas",
-  age: 50,
-  weight: 73.5,
-  isSubscribed: true,
+//parametros(parameters)
+const sum = function (number1, number2) {
+  console.log(number1 + number2);
 };
 
-console.log(typeof student);
+sum(2, 3);// arguments - argumentos de uma função executada.
 
-console.log(
-  `${student.name} tem idade de ${student.age} pesa ${student.weight} kg.`
-);
 
-/*
-  5. Declare uma variavel do tipo Array, de nome students e atribua a ela nenhum valor,
-  ou seja, somente o Array vazio.
-
-*/
